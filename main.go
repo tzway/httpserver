1ff637e39bab15b669d1938f03fed2d9ed8604f1
tzway <zhengyang.c@outlook.com>
2025-01-19 18:05:33 -0500

    add index.html and a file server handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	serverMux := http.NewServeMux()
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{Handler: serverMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
