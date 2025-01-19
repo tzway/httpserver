333d9fdc9c2f9e549afa05d3553e5c3cb86a3b17
tzway <zhengyang.c@outlook.com>
2025-01-19 18:26:35 -0500

    use http.Server pointer

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "net/http"
 func main() {
 	serverMux := http.NewServeMux()
 	serverMux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{Handler: serverMux, Addr: ":8080"}
+	server := &http.Server{Handler: serverMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
