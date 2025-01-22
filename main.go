66df566ad5d1ae5ba499647b807cc85c4b1e9eb5
tzway <zhengyang.c@outlook.com>
2025-01-21 20:31:53 -0500

    add a health checker

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import "net/http"
 
 func main() {
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
-	server := &http.Server{Handler: serverMux, Addr: ":8080"}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", handler)
+
+	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+// handler is a simple http handler that returns a 200 status code without no requirement of the request
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
