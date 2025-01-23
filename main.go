c4debcce2f3e9b3b48680fde2ac91f8973909705
tzway <zhengyang.c@outlook.com>
2025-01-23 13:02:39 -0500

    add a hit counter

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
+	fileSever := http.FileServer(http.Dir("."))
+	var cfg *apiConfig = &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(fileSever)))
 
-	mux.HandleFunc("/healthz", handler)
+	mux.HandleFunc("/metrics", cfg.handleMetrics)
+	mux.HandleFunc("/reset", cfg.handleReset)
+
+	mux.HandleFunc("/healthz", handleHealthz)
 
 	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
 
-// handler is a simple http handler that returns a 200 status code without no requirement of the request
-func handler(w http.ResponseWriter, r *http.Request) {
+// a http handler function that returns a 200 status code without no requirement of the request
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+// a middleware that increments the fileserverHits counter
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 }
